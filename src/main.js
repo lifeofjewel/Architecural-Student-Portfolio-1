5fdec765f495dd036ea0bf593b82b28275bec5e5
Julia Law <lifeofjewel@gmail.com>
2025-01-18 10:43:23 -0700

    wow

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import "./style.css";
 import data from "./data/data";
 import { createThreeScene } from "./threeScene";
 
-const Prj1s = [
+const prj1s = [
   "./DSGN 311/axon details - makeup.jpg",
   "./DSGN 311/Axon Rendered.jpg",
   "./DSGN 311/axon details - outlet.jpg",
   "./DSGN 311/render for the first page.jpg",
 ];
 
-const Prj2s = [
+const prj2s = [
   "./treeDrawings/ecological plan 1to5000.png",
   "./treeDrawings/ecological plan 1to10000.png",
   "./treeDrawings/Julia - Mind Map.jpg",
